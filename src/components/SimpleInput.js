3c0ee29b50ff3c51992d8944d8ac6d162712c171
Taneros <fricentaurus@GMAIL.COM>
2022-11-05 17:47:33 +0300

    feat: add validation on each keystroke, on Blur, on Submit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,23 +1,63 @@
-import React, {useRef, useState} from 'react';
+import React,{useRef,useState} from 'react';
 
 const SimpleInput = (props) => {
+  const nameInputRef = useRef(null)
   const [enteredName, setEnteredName] = useState('')
-  
-  const handleNameInput = (event) => {
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+  const handleNameInputBlur = (event) => {
+    setEnteredNameTouched(true)
+
+    
+    if(nameInputRef.current.value.trim() === '') {
+      setEnteredNameIsValid(false)
+    }
+    
+  }
+
+  const handleNameInputChange = (event) => {
     setEnteredName(event.target.value)
+
+        
+    
+    
+    
+    if(nameInputRef.current.value.trim() !== '') {
+      setEnteredNameIsValid(true)
+      
+    }
   }
 
   const handleFormSubmit = (event) => {
     event.preventDefault()
+    
+    setEnteredNameTouched(true)
 
-    console.log(`enteredName`,enteredName)
+    if(nameInputRef.current.value.trim() === '') {
+      setEnteredNameIsValid(false)
+      return
+    }
+    
+    
+    setEnteredNameIsValid(true)
+    setEnteredName(nameInputRef.current.value)
+    
+    console.log(`enteredName`, enteredName)
+    
+    nameInputRef.current.value = ''
   }
+
+  const nameInputIsValid = !enteredNameIsValid && enteredNameTouched
+
+  const nameInputClasses = nameInputIsValid ? 'form-control invalid' :'form-control'
 
   return (
     <form onSubmit={handleFormSubmit}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={handleNameInput}/>
+        <input type='text' id='name' ref={nameInputRef} onBlur={handleNameInputBlur} onChange={handleNameInputChange}/>
+        {nameInputIsValid && <p className='error-text'>Name must not be empty!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
