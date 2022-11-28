6f2be64ecdae08da22fdb79ef8ea954fffd176c4
Taneros <fricentaurus@GMAIL.COM>
2022-11-28 09:40:31 +0300

    refactor: optimise custom hook

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 
 const useInput = (validateValueCallback) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -9,18 +9,18 @@ const useInput = (validateValueCallback) => {
 
   const hasError = !valueIsValid && isFormTouched;
 
-  const handleInputValueChange = (event) => {
+  const handleInputValueChange = useCallback((event) => {
     setEnteredValue(event.target.value);
-  };
+  }, []);
 
-  const handleInputBlur = () => {
+  const handleInputBlur = useCallback(() => {
     setIsFormTouched(true);
-  };
+  }, []);
 
-  const resetForm = () => {
+  const resetForm = useCallback(() => {
     setEnteredValue('');
     setIsFormTouched(false);
-  };
+  }, []);
 
   return {
     value: enteredValue,
